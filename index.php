df0b7d81216761f13566328c17ec7671c59de248
Braga <guhdobraga@gmail.com>
2024-03-15 13:22:15 -0300

    Second Commit: Header Added

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lunch Fit</title>
+    <link rel="stylesheet" href="css/01estilo.css">
+</head>
+<body>
+
+<!--Header --->
+    <section class="main-header">
+        <div class="esq">
+            <div class="header-logo">
+                <img src="img/logo.png">
+            </div>
+        </div>
+        <div class="meio">
+           
+                <ul class="header-text">
+                    <li>Lanches</li>
+                    <li>Receitas</li>
+                </ul>
+           
+        </div>
+        <div class="dir">
+            <div class="user-icon">
+                <img src="img/user-ico.png">
+            </div>
+        </div>
+<!--Header --->
+</body>
+</html>
