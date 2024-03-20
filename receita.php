d4c1c66a0fd5e1876128588c9076ff118cf8be12
Maria Eduarda <duda.akiyama@gmail.com>
2024-03-20 13:05:02 -0300

    as

---
+++ receita.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lunch fit</title>
+    <link rel="stylesheet" href="css/02estilo.css">
+
+</head>
+
+<body>
+    <!--Header --->
+    <section class="main-header">
+        <div class="esq">
+            <div class="header-logo">
+                <img src="img/logo.png">
+            </div>
+        </div>
+
+        <div class="meio">
+
+
+            <ul class="header-text">
+                <li><a href="lanches.php">Lanches</a></li>
+                <li><a href="receitas.php">Receitas</a></li>
+            </ul>
+
+        </div>
+        <div class="dir">
+            <div class="user-icon">
+                <img src="img/user-ico.png">
+            </div>
+        </div>
+    </section>
+    <!--Header --->
+    <br>
+    <div class="texto">
+        <p>Receitas</p>
+    </div>
+
+    <div class="geral">
+        <div class="first">
+            <div class="imagem">
+                <img src="img/frango.jpg" alt="Image" height="201" width="305">
+                <a class="sem-cor" href="index.php"> Leia mais.....
+                    <div class="escrita">
+<br>                        
+        <h2>Sanduíche de frango </h2> 
+        <br><br>
+        <p>O sanduíche de frango é uma delícia irresistível que conquista paladares ao redor do mundo. <br> Com sua combinação de sabores e texturas, é uma opção versátil e satisfatória para 
+            qualquer refeição. <br> O segredo para um sanduíche de frango perfeito está na qualidade dos ingredientes e na preparação cuidadosa.</p>
+                </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="geral2">
+        <div class="first">
+            <div class="imagem">
+                <img src="img/peixe.jpg" alt="Image" height="201" width="305">
+                <a class="sem-cor" href="cadastroeq.php"> Leia mais.....
+                    <div class="escrita">
+<br>
+        <h2>Sanduíche de salmão </h2>     
+        <br><br>
+        <p>O sanduíche de salmão é uma delícia que combina sabores frescos e saudáveis em cada mordida. <br>
+            Começando com uma base de pão macio e leve, este sanduíche ganha vida com camadas generosas de salmão <br>
+            defumado ou grelhado, oferecendo um sabor rico e uma textura suculenta. 
+            <br>Acompanhe a receita.....</p>               
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div class="geral3">
+        <div class="first">
+            <div class="imagem">
+                <img src="img/queijo-branco.jpg" alt="Image" height="201" width="305">
+                <a class="sem-cor" href="cadastroeq.php"> Leia mais.....
+                    <div class="escrita"> 
+<br>
+        <h2>Sanduíche de queijo branco </h2>     
+<br>        
+        <p>O sanduíche de salmão é uma delícia que combina sabores frescos e saudáveis em cada mordida. <br>
+            Começando com uma base de pão macio e leve, este sanduíche ganha vida com camadas generosas de salmão <br>
+            defumado ou grelhado, oferecendo um sabor rico e uma textura suculenta. 
+            <br>Acompanhe a receita.....</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    
+</body>
+
+</html>
