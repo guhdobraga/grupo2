3cdfa5e3d7991d629703360119dd7156c402bd93
Braga <guhdobraga@gmail.com>
2024-03-22 13:53:57 -0300

    Index Added Again

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lunch Fit | Página Inicial</title>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/responsive-index.css">
+    <link rel="shortcut icon" href="img/logo lunch fit.png" type="image/png">
+</head>
+<body>
+
+<!--Header --->
+<section class="main-header">
+        <div class="esq">
+            <div class="header-logo">
+                <a href="index.php"><img src="img/logo.png"></a>
+            </div>
+        </div>
+        <div class="meio">
+           
+                <ul class="header-text">
+                    <li><a href="lanches.php">Lanches</a></li>
+                    <li><a href="receitas.php">Receitas</a></li>
+                </ul>
+           
+        </div>
+        <div class="dir">
+            <div class="user-icon">
+            <a href="login.php"><img src="img/user-ico.png"></a>
+            </div>
+        </div>
+</section>
+<!--Header --->
+
+<section class="main-content">
+    
+<!--Banner --->
+    <div class="banner">
+        <div class="banner-content">
+<!-- Colocar a imagem aqui:--><img src="img/banner.png">
+        </div>
+    </div>
+
+</section>
+<!--Banner --->
+
+<br><br>
+
+<section class="second-content flex">
+    
+<!--Texto --->
+    <div class="text-box">
+        <div class="text-content">
+            <h1>A importância dos lanches saudáveis ao longo do dia. Todos sabemos que a dieta e o estilo de vida são super importantes e têm uma enorme influência na nossa saúde e felicidade.</h1>
+        </div>
+    </div>
+<!--Texto --->
+
+<!--Imagem 1 --->
+    <div class="img-box">
+        <div class="img-content">
+            <img src="img/lanche.jfif">
+        </div>
+    </div>
+<!--Imagem 1 --->
+
+<!--Imagem 2 --->
+    <div class="img-box">
+        <div class="img-content">
+            <img src="img/lanche2.jfif">
+        </div>
+    </div>
+<!--Imagem 2 --->
+</section>
+
+<br><br><br>
+
+<!--Footer --->
+<section class="main-footer">
+        <div class="esq esq-2">
+            <div class="footer-logo">
+                <img src="img/logo.png">
+            </div>
+        </div>
+        <div class="meio">
+           
+                <h1 class="footer-text">
+                All rights reserved by Lunch Fit©
+                </h1>
+           
+        </div>
+       
+        </div>
+</section>
+<!--Footer --->
+
+</body>
+</html>
